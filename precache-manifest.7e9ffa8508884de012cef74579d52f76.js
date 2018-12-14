b0e229e9677624ff86ed66e79d55242c2bab6bcb
İbrahim Telman <cheriorn@gmail.com>
2018-12-14 17:16:50 +0300

    Updates

---
+++ precache-manifest.7e9ffa8508884de012cef74579d52f76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3bd7c15c54a047eaefd4",
+    "url": "/static/js/main.3bd7c15c.chunk.js"
+  },
+  {
+    "revision": "ed77ef6b7f6d130d216b",
+    "url": "/static/js/1.ed77ef6b.chunk.js"
+  },
+  {
+    "revision": "3bd7c15c54a047eaefd4",
+    "url": "/static/css/main.5e9d606f.chunk.css"
+  },
+  {
+    "revision": "af39987b61f5825c15410b02c6ac4922",
+    "url": "/index.html"
+  }
+];
